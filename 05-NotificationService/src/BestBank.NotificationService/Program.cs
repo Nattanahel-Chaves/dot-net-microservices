0d0f1bc00732d4f070e968238a96cf63a3f82e69
Nattanahel Chaves <nattanahel.chaves@3pillarglobal.com>
2023-09-04 16:44:31 -0600

    Fixing typo and swagger dependency

--- 05-NotificationService/src/BestBank.NotificationService/Program.cs
+++ 05-NotificationService/src/BestBank.NotificationService/Program.cs
@@ -5,10 +5,7 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-   {
-       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
-   });
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
