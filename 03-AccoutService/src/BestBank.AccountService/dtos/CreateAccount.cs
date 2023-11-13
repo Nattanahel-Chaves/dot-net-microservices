930594f45813028c609ebd8231715e7f6e6194a8
Nattanahel Chaves <nattanahel.chaves@3pillarglobal.com>
2023-11-13 10:41:23 -0600

    Refactoring

--- 03-AccoutService/src/BestBank.AccountService/dtos/CreateAccount.cs
+++
@@ -1,5 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace BestBank.AccountService.dtos;
-
-public record CreateAccount ([Required] string FirstName, [Required]string LastName, [Range(0,500)]decimal Balance);
--- 03-AccoutService/src/BestBank.AccountService/dtos/UpdateAccount.cs
+++
@@ -1,5 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace BestBank.AccountService.dtos;
-
-public record UpdateAccount ([Required]string FirstName, [Required]string LastName, [Range(0,500)]decimal Amount);
